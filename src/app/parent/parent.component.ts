d4a2bc5132abccaeacdb08d50af2078b235c18bc
Shaikh Saifuddin <shaikh.saifuddin@xebia.com>
2022-06-21 20:56:35 +0530

    few ng function added

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -8,12 +8,29 @@ import { Component, OnInit, ViewChild, ViewChildren } from '@angular/core';
 export class ParentComponent implements OnInit {
  
   apples:any=[];
-  constructor() { }
+  constructor() { 
+    console.log("1.Constructor Called 🍾🍾🍾");
+  }
+
+  ngOnChanges(){  
+    console.log("2.OnChanges Called 🥂🥂🥂");
+  }
 
   ngOnInit(): void {
     this.apples=["🍎","🍎","🍎"];
+    console.log("3.OnInit Called 🌏🌏🌏");
+  }
+
+  ngDoCheck(){
+    console.log("4.DoCheck Called ✅✅✅");
+  }
+
+  ngContentInit(){
+    console.log("5.ContentInit Called 🎛️🎛️🎛️");
   }
 
   ngAfterViewInit(){
+    console.log("6.AfterViewInit Called 🕑🕑🕑");
  }
+
 }
